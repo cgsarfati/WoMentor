99720de8f5d664304886a3b0c16f35725ab3269e
vagrant <vagrant@vagrant.vm>
2017-09-30 20:38:55 +0000

    Added load_locations

--- seed.py
+++ seed.py
@@ -7,13 +7,13 @@ from model import connect_to_db, db
 import random
 #*****************************************************************************#
 
-LEVELS = ["beginner", "intermediate", "advanced"]
-LOCATIONS = ["San Francisco", "East Bay", "South Bay", "North Bay", "Peninsula"]
-LANGUAGES = ["English", "Spanish", "Mandarin"]
-DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-ACTIVITIES = ["whiteboarding", "interview practice", "code review", "general info"]
+# LEVELS = ["beginner", "intermediate", "advanced"]
+# LOCATIONS = ["San Francisco", "East Bay", "South Bay", "North Bay", "Peninsula"]
+# LANGUAGES = ["English", "Spanish", "Mandarin"]
+# DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+# ACTIVITIES = ["whiteboarding", "interview practice", "code review", "general info"]
 
-def load_users(file):
+def load_users(file, num):
     """Load fake mentees and mentors for testing from file"""
 
     # if this file is run more than once, deletes prior data
@@ -22,24 +22,62 @@ def load_users(file):
     for row in open(file):
         row = row.rstrip()
         user_id, bio, name, email, nickname, job_title = row.split(",")
+        
+        # locations = random.choice(LOCATIONS)
+        # languages = random.choice(LANGUAGES)
+        # days = random.choice(DAYS)
+        # activities = random.choice(ACTIVITIES)
 
-        level = random.choice(LEVELS)
-        locations = random.choice(LOCATIONS)
-        languages = random.choice(LANGUAGES)
-        days = random.choice(DAYS)
-        activities = random.choice(ACTIVITIES)
-
-
-        user = User(user_id=user_id, bio=bio, name=name, email=email,
-                    nickname=nickname, job_title=job_title)
+        user = User(user_id=user_id, role_id=num, bio=bio, name=name,
+                    email=email, nickname=nickname, job_title=job_title)
 
         db.session.add(user)
         db.session.commit()
-    # random.choice
 
-    # load random availability
+def load_levels():
+    """Load fake levels for all users"""
+    
+    users = db.session.query(User.user_id).all()
+    level_id = db.session.query(Levels.level_id).all()
 
-    # make 2, load mentors and load mentees so we have an even number of both
+    for user in users:
+        # get a random level id
+        user_level_id = random.choice(level_id)
+        # get corresponding level text
+        # user_level = db.session.query(Level.level).filter(Level.level_id=user_level_id).first()
+        level = U_Level(user_id=user, level_id=user_level_id)
+
+        db.session.add(level)
+        db.session.commit()
+
+
+def load_locations():
+    """Load fake locations for all users"""
+    
+    users = db.session.query(User.user_id).all()
+    location_id = db.session.query(Locations.loc_id).all()
+
+    for user in users:
+        # get a random location
+        user_location_id = random.choice(location_id)
+        # get corresponding location text
+        # user_location = db.session.query(Locations.location).filter(Locations.loc_id=location_id).first()
+        location = U_Location(user_id=user, loc_id=user_location_id)
+
+        db.session.add(location)
+        db.session.commit()
+
+def load_languages():
+    """Load fake languages for all users"""
+    pass
+
+def load_days():
+    """Load fake availability for all users"""
+    pass
+
+def load_activities():
+    """Load fake activities for all users"""
+    pass
 
 def set_val_user_id():
     """Set value for the next user_id after seeding database"""
@@ -65,8 +103,13 @@ if __name__ == "__main__":
     mentors = "seed_data/mock_mentees.csv"
     mentees = "seed_data/mock_mentors.csv"
 
-    load_users(mentors)
+    load_users(mentors, 1)
     print "Loaded mentors to user table"
-    load_users(mentees)
+    load_users(mentees, 2)
     print "Loaded mentees to user table"
+    load_levels()
+    print "Loaded levels for all users"
 
+    # last fuunction
+    set_val_user_id()
+    print "Seeding complete!"
