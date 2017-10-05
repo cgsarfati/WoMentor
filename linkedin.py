aef4b2c1b2cf40afdb60448678d8217e79cfc7a1
Robyn Lundin <robyn@hartung.com>
2017-10-05 12:13:28 -0700

    create linkedin.py for authenticating linkedin api and requesting user info

---
+++ linkedin.py
@@ -0,0 +1,23 @@
+"""Helper functions for using LinkedIn API and OAuth."""
+
+OAUTH_URL = "https://www.linkedin.com/oauth/v2/authorization"
+ACCESS_TOKEN_URL = "https://www.linkedin.com/oauth/v2/accessToken"
+REQUEST_DATA_URL = "https://api.linkedin.com/v1/people/~?format=json"
+
+
+def get_auth_code():
+    """Request auth code from LinkedIn API."""
+
+    pass
+
+
+def get_access_token():
+    """Exchange auth code from get_auth_code for user Access Token."""
+
+    pass
+
+
+def get_user_data():
+    """Use Access Token to make authenticaled API requests from LinkedIn."""
+
+    pass
